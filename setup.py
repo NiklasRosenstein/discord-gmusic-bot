18a0edf1510896dc4523bd7ed8660919bddbb6a2
Niklas Rosenstein <rosensteinniklas@gmail.com>
2018-12-11 22:09:59 +0100

    update version number in setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.md', encoding='utf8') as fp:
 
 setuptools.setup(
   name = 'quel',
-  version = '2.0.0.dev0',
+  version = '0.9.1',
   author = 'Niklas Rosenstein',
   author_email = 'rosensteinniklas@gmail.com',
   python_requires = '>=3.7',
